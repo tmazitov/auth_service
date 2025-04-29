a84709c4ae164a6391a8745566db90322db589b6
tmazitov <timurmazitov000@gmail.com>
2025-04-29 20:52:43 +0400

    update subject of email with verification code

--- pkg/conductor/messages/code_message.go
+++ pkg/conductor/messages/code_message.go
@@ -6,7 +6,7 @@ func NewCodeMessageInfo(code string, distEmail string) *MessageInfo {
 	return &MessageInfo{
 		TemplateName: "code_template",
 		DistEmail:    distEmail,
-		Subject:      fmt.Sprintf("%s - authorization code to log in", code),
+		Subject:      fmt.Sprintf("%s - код подтверждения на платформе Mirai.", code),
 		FieldValues: map[string]string{
 			"code": code,
 		},
